f19fd4c64312bed8df3ec243057dfd51b1b4b798
Ignacio Rossetto <ignaciorossetto@hotmail.com>
2023-03-03 11:59:56 -0300

    v1.9

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -19,7 +19,8 @@ export const login = async (req,res,next) => {
     const { password, ...other } = req.user._doc
     const access_token = generateToken(other)
     res.cookie('access_token', access_token ,{
-        domain: ".onrender.com",
+        sameSite: 'none',
+        domain: "petsitterfinde.onrender.com",
         signed: true
     }).status(200).json({
         success: true,
