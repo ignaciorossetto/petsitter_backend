f7a508098338390759748082a6d9dd3dea0314d4
Ignacio Rossetto <ignaciorossetto@hotmail.com>
2023-03-03 11:50:51 -0300

    v1.8

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -18,7 +18,10 @@ export const login = async (req,res,next) => {
     if(!req.user) return res.status(401).send('Invalid credentials')
     const { password, ...other } = req.user._doc
     const access_token = generateToken(other)
-    res.cookie('access_token', access_token).status(200).json({
+    res.cookie('access_token', access_token ,{
+        domain: ".onrender.com",
+        signed: true
+    }).status(200).json({
         success: true,
         message: 'Logged in',
         payload: other
